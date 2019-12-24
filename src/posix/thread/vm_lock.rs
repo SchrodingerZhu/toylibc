9f2f23e70437ee583dd8eaaac825359902892eda
schrodingerzhu <i@zhuyi.fan>
2019-12-24 19:14:05 +0800

    finish vm_lock.rs

--- src/posix/thread/vm_lock.rs
+++ src/posix/thread/vm_lock.rs
@@ -3,11 +3,17 @@ use crate::utils::futex::RawFutex;
 static mut VM_LOCK: RawFutex = RawFutex(0, 0);
 
 #[no_mangle]
-pub unsafe fn __vm_lock() {
+pub unsafe extern "C" fn __vm_wait() {
+    VM_LOCK.wait();
+}
+
+
+#[no_mangle]
+pub unsafe extern "C" fn __vm_lock() {
     VM_LOCK.lock();
 }
 
 #[no_mangle]
-pub unsafe fn __vm_unlock() {
+pub unsafe extern "C" fn __vm_unlock() {
     VM_LOCK.unlock();
 }
